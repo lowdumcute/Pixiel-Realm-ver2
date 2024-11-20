ccaa740b51ec036bae8a9511608b4301a23db17c
lowdumcute <148664771+lowdumcute@users.noreply.github.com>
2024-11-20 20:42:25 +0700

    Update 20/11

--- Assets/Script/Element.cs
+++ Assets/Script/Element.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Element : MonoBehaviour
 {
     public static Element insntance;
+    [SerializeField]  private TMP_Text goldText;
+    [SerializeField]  private TMP_Text starText;
 
     public static float Gold =0;
-    public static float Start=0;
+    public static float Star=0;
+    public void UpdateElement()
+    {
+        goldText.text = $"Gold: {Gold} ";
+        starText.text = $"Star: {Star} ";
+    }
 }
