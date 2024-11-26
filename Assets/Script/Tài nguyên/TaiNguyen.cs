b5304b35a148827e36d499053806cc9d70ccaf56
Ruan_Ren <souji2509@gmail.com>
2024-11-26 04:34:04 -0600

    Update Tài nguyên ngoài game

---
+++ Assets/Script/Tài nguyên/TaiNguyen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//tạo tham số cho tài nguyên
+
+[CreateAssetMenu(fileName = "NewTaiNguyen", menuName = "ScriptableObjects/TaiNguyen")]
+public class TaiNguyen : ScriptableObject
+{
+    //các chỉ số tài nguyên
+    public int Gold = 0;
+    public int MaxEnergy = 30;
+    public int Energy = 30;  
+    public int Star = 0;
+}
---
+++ Assets/Script/Tài nguyên/UI_TaiNguyen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_TaiNguyen : MonoBehaviour
+{
+    //Gán Script để lấy thông số
+    public TaiNguyen tainguyen;
+
+    [SerializeField] private TextMeshProUGUI coinText;
+    [SerializeField] private TextMeshProUGUI StartText;
+    [SerializeField] private TextMeshProUGUI EnergyinText;
+
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+    private void UpdateUI()
+    {
+        //gán chữ của tài nguyên
+        coinText.text = $"{tainguyen.Gold}";
+        StartText.text = $"{tainguyen.Star}";
+        EnergyinText.text = $"{tainguyen.Energy}/{tainguyen.MaxEnergy}";
+    }
+}
